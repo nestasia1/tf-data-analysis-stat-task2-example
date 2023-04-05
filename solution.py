df3de281b8167cf4981973f944bc6ada32b830f6
nestasia1 <109359981+nestasia1@users.noreply.github.com>
2023-04-05 03:28:59 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,11 +4,11 @@ from scipy.stats import chi2
 
 chat_id = 1022895883 # Ваш chat ID, не меняйте название переменной
 
-def solution(p: float, r: np.array) -> tuple:
+def solution(p: float, x: np.array) -> tuple:
     alpha = 1 - p
     k = 33**0.5
     n = 2 * len(x)
-    a = np.sum(np.square(r))
+    a = np.sum(np.square(x))
 
     left = (a / chi2.ppf(1 - alpha/2, n))**0.5 / k
     right = (a / chi2.ppf(alpha/2, n))**0.5 / k
